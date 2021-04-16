5384a04fd82df063d43f4839e534ed4897b9cfd9
Takuya Otani <23203070+totani-finmath@users.noreply.github.com>
2021-04-15 22:30:48 -0400

    Updated.

--- g2pp.py
+++ g2pp.py
@@ -5,27 +5,28 @@ import numpy as np
 import pandas as pd
 import scipy.integrate as itg
 from scipy.stats import norm
+from scipy import optimize as opt
 from matplotlib import pyplot as plt
+import time as tm
 
 #%% --------------------------------------------------
 # constants
 # ----------------------------------------------------
-upr_itg = 100.0
-lwr_itg = -100.0
+upr_itg = .05
+lwr_itg = -.05
+y_bar_init = 0.001
 
 #%% --------------------------------------------------
-# contracts
+# contracts sample
 # ----------------------------------------------------
-t_mat = 2.0
-t_tnr = 5.0
-X = 0.005
+t_mat = 5.0
+t_tnr = 10.0
+X = 0.01
 omega = 1
-num_grid = int(t_tnr * 2) + 1
+num_grid = int(t_tnr * 4) + 1
 ts = np.linspace(t_mat, t_mat+t_tnr, num=num_grid)
-tau = [t2 - t1 for t1, t2 in zip(ts[:-1], ts[1:])]
 # aggregate
 contract = {'grids'  : ts,
-            'periods': tau,
             'strike' : X,
             'side'   : omega}
 
@@ -40,20 +41,29 @@ def PM(t,T,r=r_flat):
 # assumed G2++ calibrated
 def P(t,T,r=r_flat):
     return np.exp(-r*(T-t))
-
+# swap rate
+def swap_rate(ts,t=0.0):
+    t_stt = ts[0]
+    t_end = ts[-1]
+    taus = [t2 - t1 for t1, t2 in zip(ts[:-1], ts[1:])]
+    denom = np.sum([taus[i]*P(t,ts[i+1]) for i in range(len(taus))])
+    numer = P(t,t_stt) - P(t,t_end)
+    return numer/denom
 
 #%% --------------------------------------------------
 # parameters initial
 # ----------------------------------------------------
-alpha1 = 0.01
-alpha2 = 0.02
-sigma1 = 0.1
-sigma2 = 0.2
-rho    = 0.1
-y_bar  = 0.001 # dammy parameter for constraint
+alpha1 = 0.001
+alpha2 = 0.002
+sigma1 = 0.002
+sigma2 = 0.003
+rho    = 0.4
 # aggregate
-params = [alpha1, alpha2, sigma1, sigma2, rho, y_bar]
+params = [alpha1, alpha2, sigma1, sigma2, rho]
 
+#%% ##################################################
+# G2++ implementation
+# ####################################################
 #%% --------------------------------------------------
 # functions: convert parameters
 # ----------------------------------------------------
@@ -69,10 +79,10 @@ def M_y_g2pp(s,t,T,alpha1,alpha2,sigma1,sigma2,rho):
     return trm1 + trm2 + trm3
 def sigma_xy_g2pp(t0,alpha,sigma):
     return sigma * np.sqrt((1.0-np.exp(-2*alpha*t0))*0.5/alpha)
-def rho_xy_g2pp(t0,alpha1,alpha2,sigma1,sigma2,rho):
-    return rho*sigma1*sigma2/(alpha1 + alpha2)/sigma1/sigma2 * (1.0 - np.exp(-(alpha1+alpha2)*t0))
+def rho_xy_g2pp(t0,alpha1,alpha2,sigma1,sigma2,rho,sigma_x,sigma_y):
+    return rho*sigma1*sigma2/(alpha1 + alpha2)/sigma_x/sigma_y * (1.0 - np.exp(-(alpha1+alpha2)*t0))
 def ci_g2pp(i,X,tau): # 1<=i<=n
-    if i is not len(tau):
+    if i is not len(tau)-1:
         return X*tau[i-1]
     else:
         return 1.0 + X*tau[i-1]
@@ -102,8 +112,8 @@ def h1_g2pp(x,y_bar,mu_x,mu_y,sigma_x,sigma_y,rho_xy):
     return (y_bar - mu_y)/sigma_y/np.sqrt(1.0-rho_xy**2) - rho_xy*(x-mu_x)/sigma_x/np.sqrt(1.0-rho_xy**2)
 def h2_g2pp(sigma_y,rho_xy,h1,B_bi):
     return h1 + B_bi*sigma_y*np.sqrt(1.0-rho_xy**2)
-def lamb_g2pp(x,i,A_i,B_ai,X,tau):
-    return ci_g2pp(i,X,tau)*A_i*np.exp(-B_ai*x)
+def lamb_g2pp(x,i,A_i,B_ai,ci):
+    return ci*A_i*np.exp(-B_ai*x)
 def kappa_g2pp(x,A_i,B_bi,mu_x,mu_y,sigma_x,sigma_y,rho_xy):
     trm = mu_y - 0.5*(1.0-rho_xy**2)*sigma_y**2*B_bi + rho_xy*sigma_y*(x-mu_x)/sigma_x
     return (-1.0)*B_bi*trm
@@ -111,13 +121,18 @@ def kappa_g2pp(x,A_i,B_bi,mu_x,mu_y,sigma_x,sigma_y,rho_xy):
 #%% --------------------------------------------------
 # optimization
 # ----------------------------------------------------
+# constraint of swaption formula
+def constraint_ybar(y,x,cs,As,B1s,B2s):
+    trms = [cs[i]*As[i]*np.exp(-B1s[i]*x-B2s[i]*y) for i in range(len(cs))]
+    return np.sum(trms) - 1.0
+# swaption formula integrand
 def swpn_integrand_g2pp(x,params,contract):
     # unpack parameters
     alpha1, alpha2 = params[0], params[1]
     sigma1, sigma2 = params[2], params[3]
-    rho, y_bar     = params[4], params[5]
+    rho = params[4]
     ts  = contract['grids']
-    tau = contract['periods']
+    tau = [t2 - t1 for t1, t2 in zip(ts[:-1], ts[1:])]
     K   = contract['strike']
     omega = contract['side']
     num_grid = len(ts)
@@ -126,15 +141,19 @@ def swpn_integrand_g2pp(x,params,contract):
     mu_y    = -M_y_g2pp(0,ts[0],ts[0],alpha1,alpha2,sigma1,sigma2,rho)
     sigma_x = sigma_xy_g2pp(ts[0],alpha1,sigma1)
     sigma_y = sigma_xy_g2pp(ts[0],alpha2,sigma2)
-    rho_xy  = rho_xy_g2pp(ts[0],alpha1,alpha2,sigma1,sigma2,rho)
+    rho_xy  = rho_xy_g2pp(ts[0],alpha1,alpha2,sigma1,sigma2,rho,sigma_x,sigma_y)
     # preparation #2
     As = [A_g2pp(ts[0],ts[i+1],alpha1,alpha2,sigma1,sigma2,rho) for i in range(num_grid-1)]
     B1s = [B_g2pp(alpha1,ts[0],ts[i+1]) for i in range(num_grid-1)]
     B2s = [B_g2pp(alpha2,ts[0],ts[i+1]) for i in range(num_grid-1)]
+    cs  = [ci_g2pp(i,K,tau) for i in range(num_grid-1)]
+    # root finding
+    y_bar = opt.root(constraint_ybar,y_bar_init,\
+                args=(x,cs,As,B1s,B2s),method='hybr').x[0]
     # preparation #3
     h1  = h1_g2pp(x,y_bar,mu_x,mu_y,sigma_x,sigma_y,rho_xy)
     h2s = [h2_g2pp(sigma_y,rho_xy,h1,B2s[i]) for i in range(num_grid-1)]
-    lambs  = [lamb_g2pp(x,i+1,As[i],B1s[i],K,tau) for i in range(num_grid-1)]
+    lambs  = [lamb_g2pp(x,i+1,As[i],B1s[i],cs[i]) for i in range(num_grid-1)]
     kappas = [kappa_g2pp(x,As[i],B2s[i],mu_x,mu_y,sigma_x,sigma_y,rho_xy) for i in range(num_grid-1)]
     # integrand
     trm1 = np.exp(-0.5*(((x-mu_y)/sigma_x)**2)) / sigma_x/np.sqrt(2.0*np.pi)
@@ -142,7 +161,7 @@ def swpn_integrand_g2pp(x,params,contract):
     trm3 = np.sum([lambs[i]*np.exp(kappas[i])*norm.cdf(-omega*h2s[i]) for i in range(num_grid-1)])
     return trm1 * (trm2 - trm3)
 
-def swpn_price(params,contract):
+def swpn_price_g2pp(params,contract):
     ts = contract['grids']
     omega = contract['side']
     integral = itg.quad(swpn_integrand_g2pp,lwr_itg,upr_itg,args=(params,contract))[0] # discard error
@@ -150,21 +169,116 @@ def swpn_price(params,contract):
 
 
 #%% --------------------------------------------------
-# workspace
+# test: pricing
 # ----------------------------------------------------
-test = swpn_price(params,contract)
+test = swpn_price_g2pp(params,contract)
 print(test)
 
-
-
 #%% --------------------------------------------------
-# test
+# test: plot
 # ----------------------------------------------------
-xs = np.linspace(-10.0, 10.0, num=1000)
+xs = np.linspace(-.05, .05, num=100)
 ys = np.array([swpn_integrand_g2pp(x,params,contract) for x in xs])
 plt.plot(xs,ys)
 
+#%% ##################################################
+# optimization part
+# ####################################################
+#%% --------------------------------------------------
+# iv/price conversions
+# ----------------------------------------------------
+# black model
+def black(S,K,T,sigma,omega):
+    d1 = (np.log(S/K) + 0.5*sigma**2*T)/sigma/np.sqrt(T)
+    d2 = (np.log(S/K) - 0.5*sigma**2*T)/sigma/np.sqrt(T)
+    return omega * (S*norm.cdf(omega*d1) - K*norm.cdf(omega*d2))
+# swaption by log-normal implied vol
+def vol_to_price_black(S,K,sigma,omega,ts):
+    tau = [t2 - t1 for t1, t2 in zip(ts[:-1], ts[1:])]
+    ann = np.sum([tau[i] * P(0.0, ts[i+1]) for i in range(len(tau))])
+    return black(S,K,ts[0],sigma,omega) * ann
+# bachelier model
+def bachelier(S,K,T,sigma,omega):
+    d1 = (S-K)/sigma/np.sqrt(T)
+    return omega*(S-K)*norm.cdf(omega*d1) + sigma*np.sqrt(T)*norm.pdf(omega*d1)
+# swaption by normal implied vol
+def vol_to_price_normal(S,K,sigma,omega,ts):
+    tau = [t2 - t1 for t1, t2 in zip(ts[:-1], ts[1:])]
+    ann = np.sum([tau[i] * P(0.0, ts[i+1]) for i in range(len(tau))])
+    return bachelier(S,K,ts[0],sigma,omega) * ann
+
+#%% --------------------------------------------------
+# import sample data
+# ----------------------------------------------------
+filename = 'matrix_normal.xlsx'
+df_vols = pd.read_excel(filename,sheet_name='vols_normal',index_col=0)
+df_wgts = pd.read_excel(filename,sheet_name='weights_normal',index_col=0)
+
+# generating data
+lst_data = []
+for idx in df_vols.index:
+    for col in df_vols.columns:
+        if df_wgts[col][idx] > 0:
+            lst_data.append([idx,col,df_vols[col][idx]/10000])
+# generate contract list
+lst_contracts = []
+for i in range(len(lst_data)):
+    mem = lst_data[i]
+    # this contract
+    num_grid = int(mem[1] * 4) + 1
+    ts = np.linspace(mem[0], mem[0]+mem[1], num=num_grid)
+    swp_atm = swap_rate(ts)
+    omega = 1
+    # swaption price
+    price = vol_to_price_normal(swp_atm,swp_atm,mem[2],omega,ts)
+    contract = {'grids'  : ts,
+                'strike' : swp_atm,
+                'side'   : omega,
+                'price'  : price}
+    lst_contracts.append(contract)
+
+
+#%% --------------------------------------------------
+# objective function
+# ----------------------------------------------------
+# objective function
+def opt_objective_g2pp(params, lst_contracts):
+    err_square = []
+    for contract in lst_contracts:
+        err = contract['price'] - swpn_price_g2pp(params,contract)
+        err_square.append(err**2)
+    return np.sqrt(np.mean(err_square))
+
 #%% --------------------------------------------------
 # test
 # ----------------------------------------------------
+#rerr = opt_objective_g2pp(params,lst_contracts)
+for cont in lst_contracts:
+    price = swpn_price_g2pp(params,cont)
+    print(cont['grids'])
+    print(price)
+
+#%% --------------------------------------------------
+# calibration
+# ----------------------------------------------------
+# optimizer
+is_calib = True
+if is_calib:
+    params_init = params.copy()
+    lst_bounds = [(1.0e-5,5.0),(1.0e-5,5.0),(1.0e-5,5.0),(1.0e-5,5.0),(-1.0,1.0)]
+    time_stt = tm.time()
+    #res = opt.minimize(opt_objective_g2pp,params_init,method='BFGS',args=(lst_contracts),bounds=lst_bounds)
+    res = opt.minimize(opt_objective_g2pp,params_init,args=(lst_contracts),bounds=lst_bounds)
+    time_end = tm.time()
+    time_exe = time_stt - time_end
+
+
+
+
+
+
+
+
+# %%
+
 
